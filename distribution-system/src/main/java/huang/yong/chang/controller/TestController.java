383b2840326935e6a4c14e092e3e7ef82488c568
fuxxkk <315768455@qq.com>
2018-12-19 15:38:44 +0800

    二维码生成工具

--- distribution-system/src/main/java/huang/yong/chang/controller/TestController.java
+++ distribution-system/src/main/java/huang/yong/chang/controller/TestController.java
@@ -1,10 +1,16 @@
 package huang.yong.chang.controller;
 
+import com.google.zxing.WriterException;
 import huang.yong.chang.base.Result;
+import huang.yong.chang.util.QrCodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequestMapping("test")
 @RestController
@@ -13,5 +19,10 @@ public class TestController {
     @GetMapping("hello")
     public Result hello(String arg) throws Exception {
         return Result.SUCCESS("hello:"+arg);
+    }
+
+    @GetMapping("qr")
+    public void testQr(HttpServletRequest request, HttpServletResponse response) throws IOException, WriterException {
+        QrCodeUtils.createQrCode(response.getOutputStream(), "abcd", 510, "jpeg");
     }
 }
